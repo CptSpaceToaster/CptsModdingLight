127bf2b96542a28417a25148c41bf81d8183c0cc
CptSpaceToaster <Lugia978@yahoo.com>
2013-12-22 10:28:24 -0500

    That should be all of the Lights_Classes

--- kovukore/asm/overriddenclasses/Lights_ExtendedBlockStorage.java
+++ kovukore/asm/overriddenclasses/Lights_ExtendedBlockStorage.java
@@ -6,17 +6,6 @@ import net.minecraft.world.chunk.NibbleArray;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 
 public class Lights_ExtendedBlockStorage extends ExtendedBlockStorage {
-	public Lights_ExtendedBlockStorage(int par1, boolean par2) {
-		super(par1, par2);
-	}
-	private int yBase;
-	private byte[] blockLSBArray;
-	private NibbleArray blockMetadataArray;
-	private NibbleArray blocklightArray;
-	private NibbleArray skylightArray;
-	
-	
-	
 	@ASMAddField
 	private NibbleArray rColorArray;
 	
@@ -26,20 +15,15 @@ public class Lights_ExtendedBlockStorage extends ExtendedBlockStorage {
 	@ASMAddField
     private NibbleArray bColorArray;
 	
-	//TODO: Replace the constructor
-	public ExtendedBlockStorage(int par1, boolean par2)
+	@ASMReplaceMethod	
+	public Lights_ExtendedBlockStorage(int par1, boolean par2)
     {
-        this.yBase = par1;
-        this.blockLSBArray = new byte[4096];
-        this.blockMetadataArray = new NibbleArray(this.blockLSBArray.length, 4);
-        this.blocklightArray = new NibbleArray(this.blockLSBArray.length, 4);
-        this.rColorArray = new NibbleArray(this.blockLSBArray.length, 4);
-        this.gColorArray = new NibbleArray(this.blockLSBArray.length, 4);
-        this.bColorArray = new NibbleArray(this.blockLSBArray.length, 4);
+		super(par1, par2);
 
-        if (par2)
-        {
-            this.skylightArray = new NibbleArray(this.blockLSBArray.length, 4);
-        }
+		//this.blockLSBArray.length should evaluate to 4096... but we don't have access
+        this.rColorArray = new NibbleArray(4096, 4);
+        this.gColorArray = new NibbleArray(4096, 4);
+        this.bColorArray = new NibbleArray(4096, 4);
     }
+	
 }
