ed35da78799f8868c2ac101fe4aedc2b717bf999
heaton84 <heaton84@gmail.com>
2014-03-09 00:04:33 -0600

    Stub Forge hooks for chunk loading/saving
    Finally figured them out

--- src/main/java/kovukore/coloredlights/src/asm/ColoredLightsCoreDummyContainer.java
+++ src/main/java/kovukore/coloredlights/src/asm/ColoredLightsCoreDummyContainer.java
@@ -2,6 +2,8 @@ package kovukore.coloredlights.src.asm;
 
 import java.util.Arrays;
 
+import yamhaven.easycoloredlights.EasyColoredLights;
+import yamhaven.easycoloredlights.blocks.CLBlocksController;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,11 +13,15 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 
 import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ColoredLightsCoreDummyContainer extends DummyModContainer
 {
@@ -27,47 +33,41 @@ public class ColoredLightsCoreDummyContainer extends DummyModContainer
 		meta.name = "Colored Lights Core";
 		meta.version = "1.0.1";
 		meta.credits = "";
-		meta.authorList = Arrays.asList("AJWGeek", "Kovu", "CptSpaceToaster");
+		meta.authorList = Arrays.asList("AJWGeek", "Kovu", "CptSpaceToaster", "heaton84");
 		meta.description = "The coremod for Colored Lights";
 	}
 		
-	
-	@Subscribe
-	public void registerThis(FMLPreInitializationEvent e)
-	{
-		
-	}
-	
-	@Subscribe
-	public void registerThis(FMLInitializationEvent e)
-	{		
-		MinecraftForge.EVENT_BUS.register(this);		
-	}
-
-	@Override
-	public boolean registerBus(EventBus bus, LoadController controller)
-	{
-		bus.register(this);
-		return true;
-	}
-	
-	@Subscribe	
-	public void Load(ChunkDataEvent event)
+    @Override
+    public boolean registerBus(EventBus bus, LoadController controller)
+    {
+    	bus.register(this);
+    	MinecraftForge.EVENT_BUS.register(this); // This causes "Unable to determine registrant mod for coloredlightscore. This is a critical error and should be impossible"
+    											 // But it's OK because it still works... ???
+    	
+        return true;
+    }	
+    
+    @Subscribe
+    public void preInit(FMLPreInitializationEvent evt) {
+    	FMLLog.info("EVENT_BUS.preInit");
+    }
+    		
+	@SubscribeEvent	
+	public void LoadChunk(ChunkDataEvent event)
 	{
 		NBTTagCompound data = event.getData();
 		Chunk chunk = event.getChunk();
 		
-		FMLLog.info("$$$ Load event (%s, %s)", chunk.xPosition, chunk.zPosition);
+		//TODO: Inject RGB nibble arrays
+		//FMLLog.info("$$$ Load event (%s, %s)", chunk.xPosition, chunk.zPosition);
 	}
 
-	@Subscribe	
-	public void Save(ChunkDataEvent event)
+	@SubscribeEvent	
+	public void SaveChunk(ChunkDataEvent event)
 	{
 		NBTTagCompound data = event.getData();
 		Chunk chunk = event.getChunk();
 
-		FMLLog.info("$$$ Save event (%s, %s)", chunk.xPosition, chunk.zPosition);
-	
-	}
-	
+		//FMLLog.info("$$$ Save event (%s, %s)", chunk.xPosition, chunk.zPosition);	
+	}	
 }
