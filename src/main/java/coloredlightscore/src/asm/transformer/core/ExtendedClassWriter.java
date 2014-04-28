8724e1db1e870495f6e6a0965baeabd12a3eb5f2
CptSpaceToaster <Lugia978@yahoo.com>
2014-04-28 12:49:23 -0400

    Revised and widened attributions

--- src/main/java/coloredlightscore/src/asm/transformer/core/ExtendedClassWriter.java
+++ src/main/java/coloredlightscore/src/asm/transformer/core/ExtendedClassWriter.java
@@ -11,9 +11,11 @@ import coloredlightscore.src.asm.transformer.core.ASMUtils.ClassInfo;
  * The ExtendedClassWriter.class was written by diesieben07, who has given 
  * express permission for its use in our code.
  * 
- * diesieben07's code has been henceforth classified under the GPL license,
- * So please take that into consideration before copying it further.
- * (Meaning you'd need permission before you copy this outright, 
+ * diesieben07's code had not been classified under the GPL license at the time
+ * before we had obtained a copy.
+ * 
+ * Please take that into consideration before copying it further.
+ * (Meaning you'd need to adopt the GPL before you copy this outright, 
  *  as it's not technically under the WTFPL)
  * 
  * Source: https://github.com/diesieben07/SevenCommons/blob/master/src/main/java/de/take_weiland/mods/commons/asm/ExtendedClassWriter.java
